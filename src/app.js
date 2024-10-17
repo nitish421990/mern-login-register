41b044012d3e56a59b2509e436e8c6f4154ae85c
nitish421990 <41812918+nitish421990@users.noreply.github.com>
2024-10-17 10:39:52 +0530

    node, mongodb,hbs, html css projject login-register

--- src/app.js
+++ src/app.js
@@ -7,6 +7,8 @@ const Register = require('./models/registers');
 require('./db/conn');
 require('./models/registers');
 const bcrypt = require('bcryptjs');
+const cookieParser = require('cookie-parser');
+const auth = require('./middleware/auth');
 
 const port = process.env.port || 3000;
 
@@ -17,20 +19,43 @@ const partials_path = path.join(__dirname, '../templates/partials');
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(static_path));
+app.use(cookieParser());
 
 app.set('view engine', 'hbs');
 app.set('views', template_path);
 hbs.registerPartials(partials_path);
-console.log("secret-key is : " + process.env.SECRET_KEY);
+
 app.get('/', (req, res) => {
     res.render('index');
+})
+app.get('/secret', auth, (req, res) => {
+    console.log(`this is my cookie : ${req.cookies.jwt}`);
+    res.render('secret');
+})
 
+app.get('/logout',auth, async (req, res) => {
+    try {
+        // for single logout
+        console.log(req.user);
+    req.user.tokens = req.user.tokens.filter((token) => {
+        return token.token !== req.token;
+    });
+
+    // logout from all devices
+    req.user.tokens = [];
+        res.clearCookie('jwt');
+        console.log('logout successfully');
+        await req.user.save();
+        res.render("login");
+    } catch (err) {
+        res.status(500).send(err);
+    }
 })
 app.get('/register', (req, res) => {
     res.render('register');
 })
 app.post('/register', async (req, res) => {
-    console.log(req.body);
+    // console.log(req.body);
     try {
         const password = req.body.password;
         const cpassword = req.body.confirmpassword;
@@ -46,7 +71,8 @@ app.post('/register', async (req, res) => {
                 confirmpassword: req.body.confirmpassword
             })
             const token= await registerEmployee.generateAuthToken();
-            console.log("token error" +token);
+            res.cookie('jwt',token,{expires:new Date(Date.now() + 5000),httpOnly:true});
+          //  console.log( cookie);
            const resitered= await registerEmployee.save();
             res.status(201).render('index');
         } else {
@@ -68,9 +94,11 @@ app.post('/login', async (req, res) => {
         const password = req.body.password;
         const checkemail = await Register.findOne({email:email});
         const isMatch = await bcrypt.compare(password, checkemail.password);
-        console.log(`password matched : ${isMatch}`);
+        //console.log(`password matched : ${isMatch}`);
         const token = await checkemail.generateAuthToken();
-        console.log(`login token generated : ${token}`);
+      //  console.log(`login token generated : ${token}`);
+        res.cookie('jwt',token,{expires:new Date(Date.now() + 500000)});
+       
         if(isMatch){
             res.status(201).render('index');
         }else{
